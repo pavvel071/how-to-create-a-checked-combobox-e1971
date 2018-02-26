61745edab6c7a88177bf7078dfaf351f98ea35d3
dxBot <DevExpressExampleBot>
2018-02-26 14:16:39 +0300

    initial commit

---
+++ CS/ComboBoxEdit_CreatingCheckedComboBox/Window1.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace ComboBoxEdit_CreatingCheckedComboBox {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VB/ComboBoxEdit_CreatingCheckedComboBox/Window1.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace ComboBoxEdit_CreatingCheckedComboBox
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
